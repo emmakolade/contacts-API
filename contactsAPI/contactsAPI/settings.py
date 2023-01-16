c63d5a791734facdaee08f1c664e750424f8ad8b
Akolade <emmakolade@gmail.com>
2023-01-16 02:20:21 +0100

    login view

--- contactsAPI/contactsAPI/settings.py
+++ contactsAPI/contactsAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+#jwt
+JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'authentication.jwt.JWTAuthentication',
+    )
+}
